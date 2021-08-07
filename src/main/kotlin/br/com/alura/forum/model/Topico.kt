5bc9d77d34286362a4b0ea257372e18ae7dd083f
Cleber Simm <clebersimm@gmail.com>
2021-08-07 07:32:45 -0300

    H2 database
    Add persistence with h2

--- src/main/kotlin/br/com/alura/forum/model/Topico.kt
+++ src/main/kotlin/br/com/alura/forum/model/Topico.kt
@@ -1,14 +1,28 @@
 package br.com.alura.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.ManyToOne
+import javax.persistence.Enumerated
+import javax.persistence.EnumType
+import javax.persistence.OneToMany
 
-data class Topico (
-        var id: Long? = null,
-        val titulo: String,
-        val mensagem: String,
-        val dataCriacao: LocalDateTime = LocalDateTime.now(),
-        val curso: Curso,
-        val usuario: Usuario,
-        val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
-        val respostas: List<Resposta> = ArrayList()
+@Entity
+data class Topico(
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	var id: Long? = null,
+	val titulo: String,
+	val mensagem: String,
+	val dataCriacao: LocalDateTime = LocalDateTime.now(),
+	@ManyToOne
+	val curso: Curso,
+	@ManyToOne
+	val usuario: Usuario,
+	@Enumerated(value = EnumType.STRING)
+	val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+	@OneToMany(mappedBy = "topico")
+	val respostas: List<Resposta> = ArrayList()
 )
